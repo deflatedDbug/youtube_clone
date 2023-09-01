822b064b73aafb7d71abd2ddee30271bfaf2510c
Subin Lebow <subinlebow@gmail.com>
2023-09-01 09:18:13 -0400

    setting up GCS

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -14,19 +14,6 @@ app.post('/process-video', (req,res) => {
     if (!inputFilePath || !outputFilePath) {
         return res.status(400).send("Bad Request: Missing file path");
     }
-
-    ffmpeg(inputFilePath)
-    .outputOption('-vf', 'scale=trunc(oh*a/2)*2:360')
-    .on('end', function() {
-        console.log("processing finished successfully");
-        res.status(200).send('processing finished successfully');
-    })
-    .on('error', function(err: any) {
-        console.log('An erorr occurred ' + err.message);
-        res.status(500).send("An error ocurred " + err.message);
-    })
-
-    .save(outputFilePath);
 });
 
 const port = process.env.PORT || 3000;
